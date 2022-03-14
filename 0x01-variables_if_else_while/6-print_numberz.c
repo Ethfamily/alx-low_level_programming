d56fb3b012949930684445f30e035a890857039f
Bethel Lemma <bethelmbe@gmail.com>
2022-03-14 08:04:23 -0700

    number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *  main int eentry void
+ *
+ *  return int true
+ */
+int main(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
+}
