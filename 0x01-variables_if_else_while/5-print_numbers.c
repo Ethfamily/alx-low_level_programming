3763c2e64256f96ad5fae399e89f1d36f3da534d
Bethel Lemma <bethelmbe@gmail.com>
2022-03-14 07:16:05 -0700

    number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -output int, entry ,input void
+ *
+ * return zero mean true
+ */
+int main(void)
+{
+	for (int i = 0; i <- 9; i++)
+	{
+		printf("%d" , i);
+	}
+	printf("\n");
+
+	return (0);
+}
