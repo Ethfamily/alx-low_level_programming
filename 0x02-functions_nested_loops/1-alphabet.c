cf65455df29617abb1729338c19e561dae2a998f
Bethel Lemma <bethelmbe@gmail.com>
2022-03-16 11:26:06 -0700

    24time

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,17 @@
 #include "main.h"
 /**
-* print_alphabet - builtin function 
-*
-* Return:Always 0 (Success)
-**/
+ * print_alphabet - prints alphapbet in lowercase
+ *
+ * Return:Always 0 (Success)
+ */
 void print_alphabet(void)
 {
 	int i;
+
 	for (i = 97; i < 123; i++)
 	{
 		char c = (char)i;
+
 		_putchar(c);
 	}
 	_putchar('\n');
