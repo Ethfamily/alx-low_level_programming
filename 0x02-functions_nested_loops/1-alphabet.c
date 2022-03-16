4cb97dea604df2751b8b61acb51ed5829f4b3cbb
Bethel Lemma <bethelmbe@gmail.com>
2022-03-16 03:58:02 -0700

    10 time

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,13 +7,13 @@
 *
 *Return:lways 0 (Success)
 */
-char print_alphabet(void);
+void print_alphabet(void);
 int main(void)
 {
 	print_alphabet();
 	return (0);
 }
-char print_alphabet(void)
+void print_alphabet(void)
 {
 	for (int i = 97; i < 123; i++)
 	{
