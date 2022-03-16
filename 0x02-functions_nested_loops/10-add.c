d02f1e1421ce8fd4bb8e1c0cc3963ff5b059ea4d
Bethel Lemma <bethelmbe@gmail.com>
2022-03-16 15:06:33 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - function that adds teo integers together
+ * @i: takes in an integer
+ * @j: takes in an integer
+ * Return: i + j result
+ */
+int add(int i, int j)
+{
+	int c = i + j;
+
+	return (c);
+}
