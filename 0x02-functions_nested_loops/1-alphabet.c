43f5f8626ab515a427c4c4147d3ae71f6f39093b
Bethel Lemma <bethelmbe@gmail.com>
2022-03-16 00:05:56 -0700

    5th

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*main-Entry point
+*
+*function print_alpha:builtin function
+*
+*function main :Entry point
+*
+*Return:lways 0 (Success)
+*/
+char print_alphabet(void);
+int main(void)
+{
+	print_alphabet(b);
+	return (0);
+}
+char print_alphabet(void)
+{
+	int = a;
+	for (a = 97; a < 124; a++)
+	{
+		putchar('%C');
+	}
+}
