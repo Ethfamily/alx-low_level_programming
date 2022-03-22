b600f89be55669ef8561bfaf21d6e0891fc83a0f
Bethel Lemma <bethelmbe@gmail.com>
2022-03-22 06:55:00 -0700

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - find te lengt of a string
+ * @s: pointer to te string to check
+ * return: len
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[i])
+		len++;
+
+	return (len);
+}
