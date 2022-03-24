bd59ebd041c1f1bffe1457d4fd31eb267556aeab
Bethel Lemma <bethelmbe@gmail.com>
2022-03-23 22:14:38 -0700

    stirng

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strncpy - to copy
+ * @dest : pointer to char
+ * @src : pointer to char
+ * @n : int
+ * Return: *dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
