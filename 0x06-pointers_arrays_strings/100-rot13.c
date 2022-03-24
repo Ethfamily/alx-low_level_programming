990c70343c495f1252d50fa9482e32ec4d47c244
Bethel Lemma <bethelmbe@gmail.com>
2022-03-24 15:50:13 -0700

    do

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,16 +10,16 @@ char *rot13(char *s)
 {
 	int i;
 	int j;
-	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char original[] = "lemmabethel";
+	char rotated[] = "bethellemma";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < 52; j++)
+		for (j = 0; j < 12; j++)
 		{
-			if (s[i] == data1[j])
+			if (s[i] == original[j])
 			{
-				s[i] = datarot[j];
+				s[i] = rotated[j];
 				break;
 			}
 		}
