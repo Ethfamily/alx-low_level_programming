00dff791fce6c5cf4d6d3b504e9ccbdbeb4a8f5e
Bethel Lemma <bethelmbe@gmail.com>
2022-03-24 17:27:06 -0700

    do

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,7 +3,7 @@
  * _strcmp - compare
  * @s1 : pointer to car
  * @s2 : pointer to char
- * Return: *dest
+ * Return: Alway 0
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -12,10 +12,10 @@ int _strcmp(char *s1, char *s2)
 
 	i = 0;
 
-	while (s1[i] == s2[i] && (s1[i] != '\0' || s2[i] != '\0'))
-	{
-		i++;
-	}
-	R = s1[i] - s2[i];
-	return (R);
+	if(i < j)
+		printf("i is less than j");
+	else if(i > j)
+		printf("i is greater than j");
+	else
+		printf(" both i and j are the same");
 }
