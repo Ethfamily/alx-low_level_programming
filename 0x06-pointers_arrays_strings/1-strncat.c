167716e9a67438233a546a5bdd4d9863b2298ad3
Bethel Lemma <bethelmbe@gmail.com>
2022-03-24 17:36:54 -0700

    done

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,26 +7,6 @@
  *
  * Return: *dest
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *s)
 {
-	int m;
-	int i;
 
-	m = 0;
-
-	for (i = 0; i < 1000; i++)
-	{
-		if (dest[i] == '\0')
-		{
-			break;
-		}
-		m++;
-	}
-
-	for (i = 0; src[i] != '\0' && i < n; i++)
-	{
-		dest[m + i] = src[i];
-	}
-	dest[m + i] = '\0';
-	return (dest);
-}
