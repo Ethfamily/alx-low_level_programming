3f1a48de67c698db0c724334f74d9da575c8626f
Bethel Lemma <bethelmbe@gmail.com>
2022-04-10 00:03:54 -0700

    c langage

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
- #include <stdio.h>
-/*see the difference in puts and printf*/
+#include <stdio.h>
+/**
+ * main - Entry point
+ * main - entry for the program to start execution
+ * see the difference in puts and printf
+ * Return - Always 0
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+}
