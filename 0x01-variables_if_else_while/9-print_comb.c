ef74dc158e110557d3e078580dfa8877dcf5ea25
Bethel Lemma <bethelmbe@gmail.com>
2022-04-17 06:38:17 -0700

    new

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		putchar(44);
+		putchar(32);
+	}
+	return (0);
+}
